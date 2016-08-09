8f61df13cbb1782f0a80a77f1d38e000b0ac6194
ichwanhs96 <ichwanharyosembodo96@gmail.com>
2016-08-09 07:51:11 +0700

    change login response

--- server/models/user-for-history.js
+++ server/models/user-for-history.js
@@ -2,6 +2,8 @@ module.exports = function(Userforhistory) {
   Userforhistory.afterRemote("login", function(context, model, next){
     Userforhistory.findOne({where:{id: context.result.userId}}, function(err, user){
       context.result.isTeacher = user.isTeacher;
+      context.result.username = user.username;
+      context.result.email = user.email;
       return next();
     });
   });
